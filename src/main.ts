e157f10f37add227527f2da6e5bb37e2011ba4ae
Gnedov Pavel <pavel@gnedov.info>
2020-10-15 23:05:03 +0700

    Increased body size

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  app.use(bodyParser.json({limit: "50mb"}))
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
